570b697df75ad70191aee8bb0c210d39ca1226b1
Raphael Torquato <raphael.martini@live.com>
2023-05-23 16:56:19 -0300

    Updating the HTTPClient
    Updating the HTTPClient

--- GroceryApp/Clients/HTTPClient.swift
+++ GroceryApp/Clients/HTTPClient.swift
@@ -41,5 +41,40 @@ struct Resource<T: Codable> {
 
 struct HTTPClient {
     
+    func load<T: Codable>(_ resource: Resource<T>) async throws -> T {
+        var request = URLRequest(url: resource.url)
+        
+        switch resource.method {
+            
+        case .get(let queryItems):
+            var components = URLComponents(url: resource.url, resolvingAgainstBaseURL: false)
+            components?.queryItems = queryItems
+            guard let url = components?.url else {
+                throw NetworkError.badRequest
+            }
+            request = URLRequest(url: url)
+            
+        case .post(let data):
+            request.httpMethod = resource.method.name
+            request.httpBody = data
+            
+        case .delete:
+            request.httpMethod = resource.method.name
+        }
+        
+        let configuration = URLSessionConfiguration.default
+        
+        let session = URLSession(configuration: configuration)
+        let (data, response) = try await session.data(for: request)
+        guard let _ = response as? HTTPURLResponse else {
+            throw NetworkError.invalidResponse
+        }
+        
+        guard let result =  try? JSONDecoder().decode(resource.modelType, from: data) else {
+            throw NetworkError.decodingError
+        }
+        return result
+    }
+    
 }
 
